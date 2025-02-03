235d90ab5f617c96290642d573fb2f3bd1954be7
MichaelSScott <michael.scott@composedsoftware.net>
2025-02-03 17:35:45 +0000

    Add command to scan code for functions and refactoring suggestions

--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 import { processTestCases } from "./index.js";
-import { generateTestConfig } from "./utils/test-config-generator.js";
+import { generateTestConfig } from "./utils/testConfigGenerator.js";
+import { scanAppFunctions } from "./utils/codeScanner.js";
 import path from "path";
 
 const args = process.argv.slice(2);
 const command = args[0];
 
+// Command: Generate test config from scanned functions
 if (command === "generate-config") {
   const appDir = args[1] || "./src";
   generateTestConfig(appDir);
-} else {
+}
+
+// Command: Scan code for functions and refactoring suggestions
+else if (command === "scan-code") {
+  const appDir = args[1] || "./src";
+  const functions = scanAppFunctions(appDir);
+
+  console.log("\n--- Discovered Functions ---");
+  functions.forEach((func) => {
+    console.log(
+      `Function: ${func.name} (${func.params.join(", ")}) in ${func.file}`
+    );
+  });
+}
+
+// Default Command: Run tests from specifications
+else {
   const inputFile = args[0];
   const outputFormat = args[1] || "xlsx";
   const configFile = args[2] || "./testConfig.js";
@@ -18,6 +36,8 @@ if (command === "generate-config") {
     console.error(
       "Usage: spec-by-example <input-file> [output-format] [config-file]"
     );
+    console.error("       spec-by-example generate-config <app-directory>");
+    console.error("       spec-by-example scan-code <app-directory>");
     process.exit(1);
   }
 
